42f34cd404205f02f65cbf2aa06748ce40370433
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2022-02-28 17:00:55 +0100

    Intitial

---
+++ src/ElulacmsServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Murrayobrien\Elulacms; 
+
+use Illuminate\Support\ServiceProvider; //
+
+class ElulacmsServiceProvider extends ServiceProvider{
+    public function boot(){
+        dd('It works.'); 
+    }
+
+    public function register(){
+
+    }
+}
