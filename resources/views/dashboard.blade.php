b544133595b47c0f2d9d2d49cca23f34986c5d34
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2022-03-01 19:50:01 +0100

    Dashboard change

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,14 @@
 <div>
     <h1>Welcome to the Elula CMS Dashboard</h1>
     <p>Create collections, page data and manage CMS users</p>
+    <input type="text" placeholder="Collection Name">
+    <button onclick="createCollection(event)">Create Collection</button>
 </div>
+<script>
+    
+    function createCollection(e){
+        e.preventDefault();
+        
+    }
+
+</script>
